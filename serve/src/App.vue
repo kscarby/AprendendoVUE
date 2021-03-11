2463680789d780362b12a5a7f83bad6c6fcf0a74
Sabrina <you@example.com>
2021-03-11 16:29:58 -0300

    primeiro projeto em vue

--- serve/src/App.vue
+++ serve/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <TesteIf/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import TesteIf from './components/TesteIf.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    TesteIf,
+    HelloWorld
   }
 }
 </script>
---
+++ src/components/NovoComponente.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Novo Componente</h1>
+</template>
+
+<script>
+export default {
+  name: "NovoComponente",
+};
+</script>
+
+<style scoped>
+
+</style>
