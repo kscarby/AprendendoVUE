f52a66137b086746585c00de0ba252910894c402
Sabrina <you@example.com>
2021-03-11 17:39:19 -0300

    css

--- src/components/TesteFor.vue
+++ src/components/TesteFor.vue
@@ -2,10 +2,12 @@
   <div class="teste-for">
     <h1>Teste FOR</h1>
     <ul>
-        <li v-for=" animal of animais" v-bind:key ="animal.id">
-            <p><strong>{{ animal.titulo }}</strong></p>
-            <img v-bind:src="animal.url" v-bind:alt="animal.nome">
-        </li>
+      <li v-for="animal of animais" v-bind:key="animal.id">
+        <p>
+          <strong>{{ animal.titulo }}</strong>
+        </p>
+        <img v-bind:src="animal.url" v-bind:alt="animal.nome" />
+      </li>
     </ul>
   </div>
 </template>
@@ -18,18 +20,28 @@ export default {
       animais: [
         {
           id: 1,
-          nome: 'Gatinho',
-          url: 'https://www.petz.com.br/blog/wp-content/uploads/2020/04/meu-primeiro-gato.jpg'
+          nome: "Gatinho",
+          url:
+            "https://www.petz.com.br/blog/wp-content/uploads/2020/04/meu-primeiro-gato.jpg",
         },
         {
           id: 2,
-          nome: 'Cachorrinho',
-          url: 'https://image.cachorrogato.com.br/thumb/1200/630/1/imagens/original/imagem858.jpg'
-        }
-      ]
-    }
-  }
+          nome: "Cachorrinho",
+          url:
+            "https://image.cachorrogato.com.br/thumb/1200/630/1/imagens/original/imagem858.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+li {
+    list-style: none;
+}
 </style>
